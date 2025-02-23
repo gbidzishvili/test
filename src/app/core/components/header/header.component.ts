0830ed2a603ab0c9f4e5d631c24bd59e17382328
Giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2025-02-23 17:28:13 +0400

    clickoutside added

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,20 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SidenavToggleService } from '../navigation/services/sidenav-toggle.service';
+import { CommonModule } from '@angular/common';
+import { ClickOutsideDirective } from '../../../shared/directives/click-outside.directive';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ClickOutsideDirective],
   templateUrl: './header.component.html',
   styles: [':host{ width: 100%;}'],
 })
 export class HeaderComponent {
   sidenavToggleService = inject(SidenavToggleService);
-
+  isMenuOpen = signal(false);
   toggleSidebar() {
     this.sidenavToggleService.sidebarVisible.set(
       !this.sidenavToggleService.sidebarVisible()
     );
     console.log('isVisible', this.sidenavToggleService.sidebarVisible());
+  }
+  toggleUserMenu() {
+    this.isMenuOpen.set(!this.isMenuOpen());
+  }
+  closeMenu() {
+    this.isMenuOpen.set(false);
   }
 }
