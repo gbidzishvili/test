5080def8a1654ac9a618a81a22f91561b6771cb2
Bidzishvili giorgi <bidzishviligiorgi7@gmail.com>
2025-02-24 05:10:42 +0400

    pagination updated

--- src/app/features/users/components/users-list/pagination/pagination.component.ts
+++ src/app/features/users/components/users-list/pagination/pagination.component.ts
@@ -15,12 +15,14 @@ import { map, Observable, tap } from 'rxjs';
   providers:[{provide:MatPaginatorIntl,useClass:PaginatorIntlService}]
 })
 export class PaginationComponent {
-  currentPage = signal(0)
-  pageSize = signal(3)
   http = inject(HttpClient)
   usersService = inject(UsersService);
+  currentPage = signal(0)
+  pageSize = signal(3)
   allUsers = this.getUsersLength()
   public users = this.loadUsers();
+
+
   getUsersLength(): Observable<number> {
     return this.http.get('http://localhost:3000/users').pipe(
       map((response: any) => response.length)
@@ -29,23 +31,15 @@ export class PaginationComponent {
   handlePage(pageEvent:PageEvent){
     this.currentPage.set(pageEvent.pageIndex)
     this.pageSize.set(pageEvent.pageSize)
-    // console.log("currentPage",this.currentPage(),this.pageSize())
     this.users = this.loadUsers();
   }
 
   private loadUsers(): Observable<any> {
     const page = this.currentPage();
     const size = this.pageSize();
-
-    console.log(`Loading users for page ${page + 1} with size ${size}`);
-
-    // Adjust API request to use the correct page and page size
     return this.http
       .get(`http://localhost:3000/users?_page=${page + 1}&_per_page=${size}`)
       .pipe(
-        tap(response => {
-          console.log('Response:', response);
-        }),
         map(response => response['data']) // Ensure that you process the response accordingly
       );
   }
